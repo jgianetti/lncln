91c432ef5c9a26eb2aae632a382ac103b0b1222a
Julian Gianetti <jgianetti@infd.edu.ar>
2015-02-03 01:30:24 -0300

    adds support for update shell scripts
    Se corren por unica vez; creando y chequeando *.sh.done

--- _bin/check_update.sh
+++ _bin/check_update.sh
@@ -1,4 +1,25 @@
-#/bin/bash
+#!/bin/bash
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd "$DIR/../"
 git pull origin master
+
+# run update shell scripts,
+# only once by checking file.sh.done
+for file in _bin/update_sh/*.sh
+do
+    # script not executed yet
+    if [ -f $file ] && [ ! -f $file.done ]; then
+        /bin/bash $file
+        touch $file.done
+    fi
+done
+
+# removed script => remove *.done
+for file in _bin/update_sh/*.done
+do
+    filename="${file%.*}"
+    # script no longer exists in repo
+    if [ -f $filename.done ] && [ ! -f $filename ]; then
+        rm $filename.done
+    fi
+done
---
+++ _bin/update_sh/2015-02-02.01hs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Sets 777 for uploads/*/*
+chmod 777 uploads
+chmod 777 uploads/*
+chmod 777 uploads/*/*
