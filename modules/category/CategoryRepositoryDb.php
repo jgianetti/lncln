4301dc206741f9b15f0ff1c1a148a835bccf2281
Julian Gianetti <jgianetti@hotmail.com>
2017-05-11 15:58:05 -0300

    be/category/del: re-set user.cat_ids + cat_names

--- modules/category/CategoryRepositoryDb.php
+++ modules/category/CategoryRepositoryDb.php
@@ -53,6 +53,28 @@ class CategoryRepositoryDb extends RepositoryDb
         $this->db->prepareExec('UPDATE `category` SET `rgt` = `rgt` - 2 WHERE `rgt` >= ?', [$row['rgt']]);
         parent::del($id);
 
+        // re-set user.cat_ids + user.cat_names
+        $this->db->prepareExec('
+            UPDATE user u
+            SET
+              cat_ids = (
+                SELECT GROUP_CONCAT(DISTINCT c.id ORDER BY c.id ASC)
+                FROM category c
+                LEFT JOIN user_category u_c ON u_c.category_id = c.id
+                WHERE u_c.user_id = u.id
+                GROUP BY u_c.user_id
+              ),
+              cat_names = (
+                SELECT GROUP_CONCAT(DISTINCT c.name ORDER BY c.id ASC)
+                FROM category c
+                  LEFT JOIN user_category u_c ON u_c.category_id = c.id
+                WHERE u_c.user_id = u.id
+                GROUP BY u_c.user_id
+              )
+              WHERE u.cat_ids LIKE "%'.$id.'%"
+            ;
+        ');
+
         $this->db->commit();
 
         return true;
